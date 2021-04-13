6f10898c6380d015adc63fba1578fe22d7a856ac
Eduardo Vedooto <vedootoeduardo@gmail.com>
2021-04-13 19:04:18 -0300

    Added functionality to buy button

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@
     const promisse = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/moviefinder/filmes");
 
     promisse.then(displayMovies);
-   // promisse.catch(error);
+    //promisse.catch(error);
 
 function displayMovies(movies) {
     const div = document.querySelector(".movies");
@@ -14,7 +14,7 @@ function displayMovies(movies) {
         <div class="movie">
             <img src="${movies.data[i].imagem}">
             <div class="title">${movies.data[i].titulo}</div>
-            <button>
+            <button id="${movies.data[i].id}" onclick="buyTicket(this)">
                 Comprar
                 <ion-icon name="cart-outline"></ion-icon>
             </button>
@@ -22,6 +22,22 @@ function displayMovies(movies) {
         `
         
     }
-    console.log(movies.data[1].imagem);
+    console.log(movies.data);
 
+}
+
+function buyTicket(id) {
+    const name = prompt("Digite seu nome");
+    const seats = prompt("Quantos lugares");
+    const info = {
+        nome: name,
+        quantidade: seats
+    }
+    const promisse = axios.post(`https://mock-api.bootcamp.respondeai.com.br/api/v2/moviefinder/filmes/${id.getAttribute("id")}/ingresso`, info)
+    promisse.then(confirmationScreen);
+    //promisse.catch(error);
+}
+
+function confirmationScreen() {
+    alert("Ingresso comprado com sucesso");
 }
