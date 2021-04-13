6098dff36276b7c05bb7d4d6c8ab1ca5eb37dbe7
Eduardo Vedooto <vedootoeduardo@gmail.com>
2021-04-13 19:10:35 -0300

    Final version

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,21 @@
-
-    const promisse = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/moviefinder/filmes");
-
-    promisse.then(displayMovies);
-    //promisse.catch(error);
+const promisse = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/moviefinder/filmes");
+promisse.then(displayMovies);
 
 function displayMovies(movies) {
     const div = document.querySelector(".movies");
-    div.innerHTML = "";
 
     for (let i = 0; i < movies.data.length; i++) {
-        
         div.innerHTML += `
-        <div class="movie">
-            <img src="${movies.data[i].imagem}">
-            <div class="title">${movies.data[i].titulo}</div>
-            <button id="${movies.data[i].id}" onclick="buyTicket(this)">
-                Comprar
-                <ion-icon name="cart-outline"></ion-icon>
-            </button>
-        </div>
-        `
-        
+            <div class="movie">
+                <img src="${movies.data[i].imagem}">
+                <div class="title">${movies.data[i].titulo}</div>
+                <button id="${movies.data[i].id}" onclick="buyTicket(this)">
+                    Comprar
+                    <ion-icon name="cart-outline"></ion-icon>
+                </button>
+            </div>
+        `   
     }
-    console.log(movies.data);
-
 }
 
 function buyTicket(id) {
@@ -35,9 +27,13 @@ function buyTicket(id) {
     }
     const promisse = axios.post(`https://mock-api.bootcamp.respondeai.com.br/api/v2/moviefinder/filmes/${id.getAttribute("id")}/ingresso`, info)
     promisse.then(confirmationScreen);
-    //promisse.catch(error);
+    promisse.catch(ticketsSoldOut);
 }
 
 function confirmationScreen() {
     alert("Ingresso comprado com sucesso");
+}
+
+function ticketsSoldOut() {
+    alert("Os ingressos para este filme estão esgotados!");
 }
